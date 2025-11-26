5c642e76d6b53f72c2d932bd480bc744f43a17de
afzalahmed81 <afzalahmed123@gmail.com>
2025-11-26 22:37:49 +0500

    Conditional rendering in JSX is used in project react-afzal

--- react-afzal/src/App.jsx
+++ react-afzal/src/App.jsx
@@ -12,6 +12,42 @@ export const App = () => {
 };
 
 const NetflixSeries = () => {
+  const name = "hollywood";
+  const rating = "8.5";
+  const summary =
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione ipsam,voluptatum magnam dolore quidem non sit ea labore nisi quis quia enimlaboriosam architecto ipsum ipsa at accusamus fugit dignissimos?";
+  let age = 15;
+  // Method 2
+  // let canWatch = "Not Available";
+  // if (age >= 18) canWatch = "watch Now";
+  // Method 3, conditional rendering using function
+  const canWatch = () => {
+    if (age >= 18) return "Watch Now";
+    return "Not available";
+  };
+  const returnGenre = () => {
+    const genre = "Action";
+    return genre;
+  };
+  // if (age < 18) {                 // This method violates DRY (dont repeat yourself)
+  //   return (
+  //     <div>
+  //       <div>
+  //         <img
+  //           alt="Movie Hollywood"
+  //           src="movie-hollywood.jpg"
+  //           width="30%"
+  //           height="30%"
+  //         />
+  //       </div>
+  //       <h2>Name: {name}</h2>
+  //       <h3>Rating: {rating} </h3>
+  //       <p>{summary}</p>
+  //       <p>Genre: {returnGenre()}</p>
+  //       <button>Not Available</button>
+  //     </div>
+  //   );
+  // }
   return (
     <div>
       <div>
@@ -22,13 +58,13 @@ const NetflixSeries = () => {
           height="30%"
         />
       </div>
-      <h2>Name: Hollywood</h2>
-      <h3>Rating 8.5</h3>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione ipsam,
-        voluptatum magnam dolore quidem non sit ea labore nisi quis quia enim
-        laboriosam architecto ipsum ipsa at accusamus fugit dignissimos?
-      </p>
+      <h2>Name: {name}</h2>
+      <h3>Rating: {rating} </h3>
+      <p>{summary}</p>
+      <p>Genre: {returnGenre()}</p>
+      {/* <button>{age >= 18 ? "Watch Now" : "Not Available"}</button> */}
+      {/* <button>{canWatch}</button> */}
+      <button>{canWatch()}</button>
     </div>
   );
 };
