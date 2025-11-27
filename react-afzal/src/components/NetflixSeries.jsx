9c06aef87b678b42466650f2b0bca25d324ebb34
afzalahmed81 <afzalahmed123@gmail.com>
2025-11-27 12:34:39 +0500

    Default and named import and export introduced in project react-afzal

---
+++ react-afzal/src/components/NetflixSeries.jsx
@@ -0,0 +1,67 @@
+const NetflixSeries = () => {
+  const name = "hollywood";
+  const rating = "8.5";
+  const summary =
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione ipsam,voluptatum magnam dolore quidem non sit ea labore nisi quis quia enimlaboriosam architecto ipsum ipsa at accusamus fugit dignissimos?";
+  let age = 15;
+  // Method 2
+  // let canWatch = "Not Available";
+  // if (age >= 18) canWatch = "watch Now";
+  // Method 3, conditional rendering using function
+  const canWatch = () => {
+    if (age >= 18) return "Watch Now";
+    return "Not available";
+  };
+  const returnGenre = () => {
+    const genre = "Action";
+    return genre;
+  };
+  // if (age < 18) {                 // This method violates DRY (dont repeat yourself)
+  //   return (
+  //     <div>
+  //       <div>
+  //         <img
+  //           alt="Movie Hollywood"
+  //           src="movie-hollywood.jpg"
+  //           width="30%"
+  //           height="30%"
+  //         />
+  //       </div>
+  //       <h2>Name: {name}</h2>
+  //       <h3>Rating: {rating} </h3>
+  //       <p>{summary}</p>
+  //       <p>Genre: {returnGenre()}</p>
+  //       <button>Not Available</button>
+  //     </div>
+  //   );
+  // }
+  return (
+    <div>
+      <div>
+        <img
+          alt="Movie Hollywood"
+          src="movie-hollywood.jpg"
+          width="30%"
+          height="30%"
+        />
+      </div>
+      <h2>Name: {name}</h2>
+      <h3>Rating: {rating} </h3>
+      <p>{summary}</p>
+      <p>Genre: {returnGenre()}</p>
+      {/* <button>{age >= 18 ? "Watch Now" : "Not Available"}</button> */}
+      {/* <button>{canWatch}</button> */}
+      <button>{canWatch()}</button>
+    </div>
+  );
+};
+
+export default NetflixSeries;
+
+export const Header = () => {
+  return <p>Movie Recomendation App</p>;
+};
+
+export const Footer = () => {
+  return <p>Developed by Afzal Ahmed</p>;
+};
