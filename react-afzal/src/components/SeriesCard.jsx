c32d18aa321a573b13abf0197a610889ac29f331
afzalahmed81 <afzalahmed123@gmail.com>
2025-12-12 11:51:53 +0500

    Conditional styling added to the project reaact-afzal

--- react-afzal/src/components/SeriesCard.jsx
+++ react-afzal/src/components/SeriesCard.jsx
@@ -8,9 +8,12 @@ export const SeriesCard = ({ data }) => {
     padding: "1.2rem 2.4rem",
     border: "none",
     fontSize: "1.6rem",
-    backgroundColor: "var(--bnt-hover-bg-color)",
+    backgroundColor: `${rating >= 8.5 ? "#7dca0" : "#f7dc6f"}`,
     color: "var(--bg-color)",
+    fontWeight: "bold",
+    cursor: "pointer",
   };
+  const ratingClass = rating >= 8.5 ? "super_hit" : "average";
   return (
     <li className="card">
       <div>
@@ -18,7 +21,9 @@ export const SeriesCard = ({ data }) => {
       </div>
       <div className="card-content">
         <h2>Name: {name}</h2>
-        <h3 style={{ margin: "1.2rem 0" }}>Rating: {rating} </h3>
+        <h3 style={{ margin: "1.2rem 0" }}>
+          Rating: <span className={`rating ${ratingClass}`}> {rating} </span>
+        </h3>
         <p style={{ margin: "1.2rem 0" }}>Summary:{description}</p>
         <p style={{ margin: "1.2rem 0" }}>Genre: {genre.join(", ")}</p>
         <p style={{ margin: "1.2rem 0" }}>Cast: {cast.join(", ")}</p>
